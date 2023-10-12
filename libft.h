e13134d6064760832aabefac9639f40d7c787de2
Andreas Gjertsen Kalicani <akalican@c1r16s6.42lausanne.ch>
2023-10-12 15:40:31 +0200

    updatet few function to header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: akalican <akalican@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 15:07:42 by andreasgjer       #+#    #+#             */
-/*   Updated: 2023/10/11 15:47:35 by akalican         ###   ########.fr       */
+/*   Updated: 2023/10/12 15:38:29 by akalican         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,8 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+int		ft_atoi(const char *str);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
 #endif
