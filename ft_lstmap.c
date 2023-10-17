e86aac3baa2f9983dc277bf3691b7178f802e39d
Andreas Gjertsen Kalicani <akalican@c1r1s2.42lausanne.ch>
2023-10-17 16:55:02 +0200

    modifed some functions

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,35 +6,49 @@
 /*   By: akalican <akalican@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:21:38 by akalican          #+#    #+#             */
-/*   Updated: 2023/10/17 13:22:19 by akalican         ###   ########.fr       */
+/*   Updated: 2023/10/17 16:44:22 by akalican         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+t_list	*ft_lstnew_strict(void *content)
+{
+	t_list	*element;
+
+	element = (t_list *)malloc(sizeof(t_list));
+	if (!element)
+	{
+		free(content);
+		return (NULL);
+	}
+	element->next = NULL;
+	element->content = content;
+	return (element);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
-	t_list	*saved;
+	t_list	*new_list;
+	t_list	*new_elem;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	new_lst = ft_lstnew(f(lst->content));
-	if (!new_lst)
-		return (NULL);
-	saved = new_lst;
-	lst = lst->next;
+	new_list = NULL;
+	new_elem = NULL;
 	while (lst)
 	{
-		new_lst->next = ft_lstnew(f(lst->content));
-		if (!new_lst->next)
+		new_elem = ft_lstnew_strict(f(lst->content));
+		if (!new_elem)
 		{
-			ft_lstclear(&saved, del);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		new_lst = new_lst->next;
+		if (!new_list)
+			new_list = new_elem;
+		else
+			ft_lstadd_back(&new_list, new_elem);
 		lst = lst->next;
 	}
-	new_lst->next = NULL;
-	return (saved);
+	return (new_list);
 }
--- ft_split.c
+++ ft_split.c
@@ -6,70 +6,111 @@
 /*   By: akalican <akalican@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:03:12 by akalican          #+#    #+#             */
-/*   Updated: 2023/10/17 14:58:27 by akalican         ###   ########.fr       */
+/*   Updated: 2023/10/17 16:53:29 by akalican         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_words(const char *str, char c)
+static size_t	count_words(char const *s, char c)
 {
-	int	i;
-	int	trigger;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
-	trigger = 0;
-	while (*str)
+	count = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i])
 	{
-		if (*str != c && trigger == 0)
-		{
-			trigger = 1;
+		while (s[i] && (s[i] == c))
 			i++;
-		}
-		else if (*str == c)
-			trigger = 0;
-		str++;
+		if (s[i])
+			count++;
+		while (s[i] && (s[i] != c))
+			i++;
 	}
+	return (count);
+}
+
+static size_t	word_len(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && (s[i] != c))
+		i++;
 	return (i);
 }
 
-static char	*word_dup(const char *str, int start, int finish)
+static char	*malloc_word(char const *s, char c)
 {
+	size_t	word_length;
+	size_t	i;
 	char	*word;
-	int		i;
 
 	i = 0;
-	word = malloc((finish - start + 1) * sizeof(char));
-	while (start < finish)
-		word[i++] = str[start++];
+	word_length = word_len(s, c);
+	word = malloc(sizeof(char) * (word_length + 1));
+	if (!word)
+		return (NULL);
+	while (i < word_length)
+	{
+		word[i] = s[i];
+		i++;
+	}
 	word[i] = '\0';
 	return (word);
 }
 
+static void	free_all(char ***result, size_t words)
+{
+	size_t	count;
+	size_t	i;
+
+	count = -1;
+	i = -1;
+	while (words > ++count)
+	{
+		if ((*result)[count] == NULL)
+		{
+			while (words > ++i)
+			{
+				if ((*result)[i])
+					free((*result)[i]);
+			}
+			free(*result);
+			*result = NULL;
+			return ;
+		}
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
+	char	**split_arr;
 	size_t	i;
-	size_t	j;
-	int		index;
-	char	**split;
+	size_t	words;
 
-	split = malloc((count_words(s, c) + 1) * sizeof(char *));
-	if (!s || !split)
-		return (0);
 	i = 0;
-	j = 0;
-	index = -1;
-	while (i <= ft_strlen(s))
+	words = count_words(s, c);
+	if (s == NULL)
+		return (NULL);
+	split_arr = malloc(sizeof(char *) * (words + 1));
+	if (!split_arr)
+		return (NULL);
+	while (*s)
 	{
-		if (s[i] != c && index < 0)
-			index = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		while (*s && *s == c)
+			s++;
+		if (*s)
 		{
-			split[j++] = word_dup(s, index, i);
-			index = -1;
+			split_arr[i++] = malloc_word(s, c);
 		}
-		i++;
+		while (*s && *s != c)
+			s++;
 	}
-	split[j] = 0;
-	return (split);
+	split_arr[i] = NULL;
+	free_all(&split_arr, words);
+	return (split_arr);
 }
